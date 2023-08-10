81fab2ac7f57ac671304bfd2b1c3e295ca2915bc
Narava Swarna Raj <snarava@msa.miraclesoft.com>
2023-08-10 13:33:29 -0400

    first commit

---
+++ Demo.js
@@ -0,0 +1,15 @@
+class test{
+	constructor(num1,num2){
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	add(){
+		console.log(this.num1+this.num2);	
+	}
+	sub(){
+		console.log(this.num1-this.num2);
+	}
+}
+let ref = new test(2,4);
+ref.add();
+ref.sub();
---
+++ sample.ps1
@@ -0,0 +1,11 @@
+﻿New-Item "D:\snarava.txt"
+
+
+
+"Hello World" | Set-Content "D:\snarava.txt"
+
+"Hello Worlds" | Add-Content "D:\snarava.txt"
+
+"Hello Worlds" | Out-File "D:\snarava.txt" -Append
+
+Get-Content  "D:\snarava.txt"
